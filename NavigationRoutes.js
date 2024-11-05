b4b9df688e9d7ea52f64c30a21666e07a8024179
Dat Truong <datg@hawaii.edu>
2024-11-04 18:41:51 -1000

    Update NavigationRoutes.js

--- NavigationRoutes.js
+++ NavigationRoutes.js
@@ -1,2 +1,17 @@
+// Create an adjacency list for each islands and its routes
 function adjList(islands, routes) {
+  let graph = {};
+
+  islands.forEach(island => {
+    graph[island] = [];
+  })
+
+  routes.forEach(route => {
+    let { src, dest, time } = route;
+    routes.forEach(route => {
+      graph[src].push({dest, time});
+    });
+  });
+
+  return graph;
 }
